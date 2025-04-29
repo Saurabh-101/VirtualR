12e057d254948c402f8664ca40ac587506eeab5d
Saurabh Gupta <saurabhgupta72dh3@gmail.com>
2025-04-29 21:56:44 +0530

    Updated dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom'
-import SignIn from "./Pages/SignIn.jsx";
-import Documentation from './Pages/Documentation.jsx'
-import Register from './Pages/Register.jsx'
-import FeatureSection from './components/FeatureSection.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignIn from './Pages/SignIn.jsx';
+import Documentation from './Pages/Documentation.jsx';
+import Register from './Pages/Register.jsx';
+import FeatureSection from './components/FeatureSection.jsx';
+
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App />,
+    path: "/",
+    element: <App />
   },
   {
-    path:"/signIn",
-    element:<SignIn/>
+    path: "/signIn",
+    element: <SignIn />
   },
   {
-    path:"/Documentation",
-    element:<Documentation/>,
+    path: "/Documentation",
+    element: <Documentation />
   },
   {
-    path:"/Register",
-    element:<Register/>,
+    path: "/Register",
+    element: <Register />
   },
   {
-    path:"/FeatureSection",
-    element:<FeatureSection/>
+    path: "/FeatureSection",
+    element: <FeatureSection />
   }
-]);
-
-
+], {
+  future: {
+    v7_startTransition: true,       
+    v7_skipActionErrorRevalidation: true,
+    v7_relativeSplatPath: true,     
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,   
+    v7_partialHydration: true       
+  }
+});
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
